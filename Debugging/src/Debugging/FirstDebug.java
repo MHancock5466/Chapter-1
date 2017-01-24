df8768587c681bd70eacba7102131b03e594ea3e
MHancock5466 <mhancock5466@gmail.com>
2017-01-24 14:01:29 -0600

    Add files via upload

---
+++ Debugging/src/Debugging/FirstDebug.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Debugging;
+
+/**
+ * @author mh137428
+ *
+ */
+public class FirstDebug 
+{
+	/* This program displays a greeting */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+	}
+
+}
