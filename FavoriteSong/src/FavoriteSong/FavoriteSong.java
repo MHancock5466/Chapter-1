8a5f8b7ab2bf371cf8e5771d8a601b65b1d763c3
MHancock5466 <mhancock5466@gmail.com>
2017-01-24 14:20:56 -0600

    Add files via upload

---
+++ FavoriteSong/src/FavoriteSong/FavoriteSong.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package FavoriteSong;
+
+/**
+ * @author mh137428
+ *
+ */
+public class FavoriteSong 
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Woke up on the right side of the bed,");
+		System.out.println("What's up with this Prince song inside my head?");
+		System.out.println("Hands up if you're down to get down tonight,");
+		System.out.println("'Cause it's always a good time.");
+	}
+
+}
