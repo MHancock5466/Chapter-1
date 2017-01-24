397ebbee7d477d521da7fedfb10e7af1913037c6
MHancock5466 <mhancock5466@gmail.com>
2017-01-24 14:15:04 -0600

    Add files via upload

---
+++ Chairs1.2/src/Chairs/Chair.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Chairs;
+
+/**
+ * @author mh137428
+ *
+ */
+public class Chair 
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("X                    X");
+		System.out.println("X                    X");
+		System.out.println("X     XXXXXXXXXX     X");
+		System.out.println("XXXXX X        X XXXXX");
+		System.out.println("X   X X        X X   X");
+		System.out.println("X   X X        X X   X");
+	}
+
+}
