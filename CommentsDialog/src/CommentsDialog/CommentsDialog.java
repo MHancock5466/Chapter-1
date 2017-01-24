c7778704b5a190125d6116d1d270cd8405d633a7
MHancock5466 <mhancock5466@gmail.com>
2017-01-24 14:40:44 -0600

    Add files via upload

---
+++ CommentsDialog/src/CommentsDialog/CommentsDialog.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package CommentsDialog;
+
+/**
+ * @author mh137428
+ *Program comments are nonexecuting statements you add to a file for the purpose of documentation.
+ */
+public class CommentsDialog 
+{
+	/*Program comments are nonexecuting statements you add to a file for the purpose of documentation.*/
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Program comments are nonexecuting statements you add to a file for the purpose of documentation.");
+		//Program comments are nonexecuting statements you add to a file for the purpose of documentation.
+	}
+
+}
