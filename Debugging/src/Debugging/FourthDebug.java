6fbdb7e697399d5a68ab92785cd7c0c04834ac1c
MHancock5466 <mhancock5466@gmail.com>
2017-01-25 13:10:29 -0600

    Add files via upload

---
+++ Debugging/src/Debugging/FourthDebug.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package Debugging;
+
+/**
+ * @author MH137428
+ *
+ */
+import javax.swing.JOptionPane;
+public class FourthDebug
+{
+  public static void main(String[] args)
+  {
+     JOptionPane.showMessageDialog(null, "First GUI program");
+  }
+}
