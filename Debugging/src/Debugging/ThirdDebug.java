183a2cd3d4343b62665c18f189ac5f270b6e0c9d
MHancock5466 <mhancock5466@gmail.com>
2017-01-25 13:04:19 -0600

    Add files via upload

---
+++ Debugging/src/Debugging/ThirdDebug.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Debugging;
+
+/**
+ * @author MH137428
+ *
+ */
+public class ThirdDebug
+{
+   public static void main(String[] argz)
+   {
+      System.out.println("Over the river");
+      System.out.println("and through the woods");
+      System.out.println("to Grandmother's house we go.");
+   }
+}
